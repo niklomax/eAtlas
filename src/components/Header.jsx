a1e25ce92c4ab0a641e1294b9e374fe3368618b7
Layik Hama <layik.hama@gmail.com>
2020-04-14 16:45:45 +0100

    branding with UoL header/footer

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,79 +2,20 @@
  * geoplumber R package code.
  */
 import React, { useState } from 'react';
-import { Navbar, Nav, NavItem } from 'react-bootstrap';
+import { Navbar, Nav } from 'react-bootstrap';
 import { Link, withRouter } from 'react-router-dom';
-
-const navs = [
-  {
-    key: 1,
-    to: 'world',
-    title: 'World'
-  },
-  {
-    key: 2,
-    to: "about",
-    title: "About"
-  },
-];
-
 function Header(props) {
   const [dark, setDark] = useState(props.dark)
   return (
     <Navbar inverse={dark} collapseOnSelect>
-      <Navbar.Header>
-        <Navbar.Brand>
+        <Nav pullRight>
           <Link to="/">
-            <span style={{color: dark ? 'white' : 'black'}}>COVID19</span>
+            <img className="logo-standard" style={{height: "46px", maxHeight:"46px"}} 
+            src="https://lida.leeds.ac.uk/wp-content/themes/lida/resources/images/uol-logo.png" />
+            <img className="logo-mobile" style={{height: "46", maxHeight:"46"}} 
+            src="https://lida.leeds.ac.uk/wp-content/themes/lida/resources/images/uol-logo-sml.png" />
           </Link>
-        </Navbar.Brand>
-        <Navbar.Toggle />
-      </Navbar.Header>
-      <Navbar.Collapse>
-        <Nav>
-          {
-            navs.map((item, i) => {
-              return (
-                <NavItem
-                  key={i}
-                  eventKey={item.key}
-                  onClick={() => props.history.push(item.to)}>
-                  {item.title}
-                </NavItem>
-              )
-            })
-          }
         </Nav>
-        {/* trick RB with navbar-nav > li a but do it like following*/}
-        {/* <div className="nav navbar-nav">
-            <li>
-              <a href="https://github.com/layik/eAtlas">
-                <i style={{ fontSize: '1.5em' }} className="fa fa-github"></i>
-              </a>
-            </li>
-          </div> */}
-        <Nav pullRight>
-          <NavItem href="https://github.com/layik/eAtlas">
-            <i style={{ fontSize: '1.5em' }} className="fa fa-github"></i>
-          </NavItem>
-        </Nav>
-        <Nav pullRight>
-          <NavItem onClick={() => {
-            typeof props.toggleTheme === 'function' && props.toggleTheme()
-            setDark(!dark)
-          }}>
-            <svg width="20" height="20" viewBox="0 0 24 24" aria-hidden="true">
-              <g transform="matrix( 1 0 0 1 4 1 )">
-                <path fillRule="evenodd" clipRule="evenodd"
-                  d="M15.3999 11C15.7999 10.1 16 9 16 8C16 3.6 12.4 0 8 0C3.6 0 0 3.6 0 8C0 9.1 0.200098 10.1 0.600098 11L2.19995 15L13.8 15L15.3999 11ZM11 22L12.6001 18L3.3999 18L5 22L11 22Z" fill={
-                    dark ? '#fff' : '#000'
-                  } opacity="1">
-                </path>
-              </g>
-            </svg>
-          </NavItem>
-        </Nav>
-      </Navbar.Collapse>
     </Navbar >
   )
 }
