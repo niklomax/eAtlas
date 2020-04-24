b6dc16044b6321035eab85e55efdc8c6c14170ae
Layik Hama <layik.hama@gmail.com>
2020-04-24 20:47:53 +0100

    Other final touches

--- src/App.js
+++ src/App.js
@@ -65,6 +65,14 @@ function App() {
             </Switch>
           </BaseProvider>
         </StyletronProvider>
+        <div className="uol-wrapper">
+					<p>© {new Date().getFullYear} University of Leeds, Leeds, LS2 9JT</p>
+					<div class="menu-footer-menu-container"><ul id="menu-footer-menu" className="menu"><li id="menu-item-53" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-53"><a target="_blank" rel="noopener noreferrer" href="http://www.leeds.ac.uk/termsandconditions">Terms &amp; Conditions</a></li>
+          <li id="menu-item-54" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-54"><a target="_blank" rel="noopener noreferrer" href="http://www.leeds.ac.uk/accessibility">Accessibility</a></li>
+          <li id="menu-item-487" className="menu-item menu-item-type-post_type menu-item-object-page menu-item-487"><a href="https://lida.leeds.ac.uk/privacy/">Privacy</a></li>
+          <li id="menu-item-484" className="menu-item menu-item-type-custom menu-item-object-custom menu-item-484"><a target="_blank" rel="noopener noreferrer" href="http://www.leeds.ac.uk/foi">Freedom of Information</a></li>
+          </ul></div>
+				</div>
       </main>
     )
 }
